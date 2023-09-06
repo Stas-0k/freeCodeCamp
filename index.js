0c70dfafe17cd910f23735b2db4611a4bfff480e
Stanislav <brickbybrick15@gmail.com>
2023-09-06 15:28:10 +0300

    final
    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // function sumAll(arr) {
 // let max = Math.max(arr[0], arr[1])
 // let min = Math.min(arr[0], arr[1])
-// let result =0 
+// let result =0
 // for (let i=min; i<=max; i+=1) {
 //     result+=i;
 // }
@@ -19,7 +19,6 @@
 // // Змініть код лише над цим рядком
 // console.log(myArray);
 
-
 // function mutation(arr) {
 //     const test = arr[1].toLowerCase();
 //     const target = arr[0].toLowerCase();
@@ -28,51 +27,72 @@
 //     }
 //     return true;
 //   }
-  
-//   mutation(["hello", "hey"]);
 
+//   mutation(["hello", "hey"]);
 
 // function towerBuilder(nFloors) {
 //   arr=[]
 //   for(let i =0; i<=nFloors; i+=1) {
-//     arr.push('*')
+//     arr.push('*')"02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"ios
 //     console.log(arr)
 //   }
 // }
 
-// towerBuilder(4)urs = 
-
-
+// towerBuilder(4)urs =
 
 function stat(strg) {
-let arr1=[]
-arr1=strg.split(',')
-let arrFin =[]
+  
+  let arr1 = [];
+  arr1 = strg.split(",");
+  let arrFin = [];
 
-for(let i =0; i<arr1.length; i++) {
-  arrNew = arr1[i].split('|')
-  let seconds = Number(arrNew[0])*60*60+Number(arrNew[1])*60+Number(arrNew[2])
-  arrFin.push(seconds)
+  for (let i = 0; i < arr1.length; i++) {
+    arrNew = arr1[i].split("|");
+    let seconds =
+      Number(arrNew[0]) * 60 * 60 + Number(arrNew[1]) * 60 + Number(arrNew[2]);
+    arrFin.push(seconds);
+  }
+
+  arrFin.sort(function(a,b){return a-b});
+
+  let sum = 0;
+
+  for (let i = 0; i < arrFin.length; i++) {
+    sum += arrFin[i];
+  }
+
+  let minResult = Math.min.apply(null, arrFin);
+  let maxResult = Math.max.apply(null, arrFin);
+  let range = maxResult - minResult;
+  let avg = sum / arrFin.length;
+
+  let med=0;
+
+  if(arrFin.length%2===0) {
+    med = (arrFin[arrFin.length/2]+arrFin[arrFin.length/2-1])/2
+  } else {
+    med = arrFin[Math.ceil(arrFin.length/2-1)]
+  }
+
+  
+
+  // console.log(arrFin);
+  // console.log(secToDate(range));
+  // console.log(secToDate(avg));
+  // console.log(med)
+
+  return `Range: ${secToDate(range)} Average: ${secToDate(avg)} Median: ${secToDate(med)}`
 }
 
-let minResult = Math.min.apply(null, arrFin)
-let maxResult = Math.max.apply(null, arrFin)
-let range = maxResult-minResult
 
-// let hours = Math.floor(range%3600)
-// hours >=3600 ? console.log('ok') : console.log('notok')
-// let minutes = Math.floor(range / 60);
-// let seconds = range % 60;
-// console.log(range)
-// console.log(hours)
-// console.log(minutes)
-// console.log(seconds)
+const secToDate = (seconds) => {
+  if(seconds===''|| NaN) {return} else {
+  const date = new Date(null);
+  
+  date.setSeconds(seconds);
+  const result = date.toISOString().slice(11, 19).replace(':', '|').replace(':', '|');
+  return result;
+  }
+};
 
-const date = new Date(null);
-date.setSeconds(range); // specify value for SECONDS here
-const result = date.toISOString().slice(11, 19);
-console.log(result)
-
-}
-
-stat("01|15|59, 1|47|6, 01|17|20, 1|32|34, 2|3|17")
+console.log(stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"))
